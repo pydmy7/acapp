d74857c22fcac88d1ce0f05e53df5aa33d20136e
wyy <1822364545@qq.com>
2022-07-06 16:06:53 +0800

    implement multi mode and blink and coldtime

--- game/static/js/src/playground/socket/multiplayer/zbase.js
+++ game/static/js/src/playground/socket/multiplayer/zbase.js
@@ -2,7 +2,7 @@ class MultiPlayerSocket {
     constructor(playground) {
         this.playground = playground;
 
-        this.ws = new WebSocket("wss://app2479.acapp.acwing.com.cn/wss/multiplayer/");
+        this.ws = new WebSocket("wss://app165.acapp.acwing.com.cn/wss/multiplayer/");
 
         this.start();
     }
@@ -22,8 +22,14 @@ class MultiPlayerSocket {
             let event = data.event;
             if (event === "create_player") {
                 outer.receive_create_player(uuid, data.username, data.photo);
-            } else if (event == "move_to") {
+            } else if (event === "move_to") {
                 outer.receive_move_to(uuid, data.tx, data.ty);
+            } else if (event === "shoot_fireball") {
+                outer.receive_shoot_fireball(uuid, data.tx, data.ty, data.ball_uuid);
+            } else if (event === "attack") {
+                outer.receive_attack(uuid, data.attackee_uuid, data.x, data.y, data.angle, data.damage, data.ball_uuid);
+            } else if (event === "blink") {
+                outer.receive_blink(uuid, data.tx, data.ty);
             }
         };
     }
@@ -40,11 +46,12 @@ class MultiPlayerSocket {
 
     get_player(uuid) {
         let players = this.playground.players;
-        for (let i = 0; i < players.length; ++i) {
+        for (let i = 0; i < players.length; i ++ ) {
             let player = players[i];
             if (player.uuid === uuid)
                 return player;
         }
+
         return null;
     }
 
@@ -71,7 +78,7 @@ class MultiPlayerSocket {
             'event': "move_to",
             'uuid': outer.uuid,
             'tx': tx,
-            'ty': ty
+            'ty': ty,
         }));
     }
 
@@ -83,7 +90,7 @@ class MultiPlayerSocket {
         }
     }
 
-    send_shoot_fireball(tx, ty, uuid) {
+    send_shoot_fireball(tx, ty, ball_uuid) {
         let outer = this;
         this.ws.send(JSON.stringify({
             'event': "shoot_fireball",
@@ -92,5 +99,53 @@ class MultiPlayerSocket {
             'ty': ty,
             'ball_uuid': ball_uuid,
         }));
+    }
+
+    receive_shoot_fireball(uuid, tx, ty, ball_uuid) {
+        let player = this.get_player(uuid);
+        if (player) {
+            let fireball = player.shoot_fireball(tx, ty);
+            fireball.uuid = ball_uuid;
+        }
+    }
+
+    send_attack(attackee_uuid, x, y, angle, damage, ball_uuid) {
+        let outer = this;
+        this.ws.send(JSON.stringify({
+            'event': "attack",
+            'uuid': outer.uuid,
+            'attackee_uuid': attackee_uuid,
+            'x': x,
+            'y': y,
+            'angle': angle,
+            'damage': damage,
+            'ball_uuid': ball_uuid,
+        }));
+    }
+
+    receive_attack(uuid, attackee_uuid, x, y, angle, damage, ball_uuid) {
+        let attacker = this.get_player(uuid);
+        let attackee = this.get_player(attackee_uuid);
+
+        if (attacker && attackee) {
+            attackee.receive_attack(x, y, angle, damage, ball_uuid, attacker);
+        }
+    }
+
+    send_blink(tx, ty) {
+        let outer = this;
+        this.ws.send(JSON.stringify({
+            'event': "blink",
+            'uuid': outer.uuid,
+            'tx': tx,
+            'ty': ty,
+        }));
+    }
+
+    receive_blink(uuid, tx, ty) {
+        let player = this.get_player(uuid);
+        if (player) {
+            player.blink(tx, ty);
+        }
     }
 }
