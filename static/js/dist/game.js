39936aff4eb0a3e03ef5262df7fe7698fac480a8
wyy <1822364545@qq.com>
2022-07-22 16:39:21 +0800

    description

--- static/js/dist/game.js
+++ static/js/dist/game.js
@@ -26,6 +26,12 @@ class AcGameDescription {
         this.$description = $(`
 <div class = "ac-game-description">
     <div class = "ac-game-description-title">
+        HR速来！我要找工作！！！
+    </div>
+    <div class = "ac-game-description-title">
+        背景音乐：星瞳 - Shining You
+    </div>
+    <div class = "ac-game-description-title">
         操作说明
     </div>
     <div class = "ac-game-description-content">
@@ -33,19 +39,33 @@ class AcGameDescription {
         <p>鼠标左键：朝目标方向使用技能</p>
         <p>Q：发射火球</p>
         <p>F：闪现</p>
-        <p>Enter：打开聊天框</p>
+        <p>Enter：问候家人</p>
         <p>ECS：关闭聊天框</p>
+    </div>
+    <div class="ac-game-description-return">
+        返回
     </div>
 </div>
 `);
 
-        this.hide();
+        this.$return = this.$description.find('.ac-game-description-return');
+
+        this.$description.hide();
         this.root.$ac_game.append(this.$description);
 
         this.start();
     }
 
     start() {
+        this.add_listening_events();
+    }
+
+    add_listening_events() {
+        let outer = this;
+        this.$return.click(function() {
+            outer.hide();
+            outer.root.menu.show();
+        });
     }
 
     show() {  // show description interface
